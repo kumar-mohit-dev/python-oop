ed792c0b551fd3bb14fb5a340f1dce12319abe6e
Mohit Kumar <mohitgoswami.work@gmail.com>
2024-08-10 23:24:08 +0530

    Added contents

---
+++ python-oops.py
@@ -0,0 +1,18 @@
+#Contents:-
+#Python OBject Oriented Programming intro
+#class variables
+#inheritance
+#multiple inheritance
+#abstract classes
+#super()
+#polymorphism
+#duck typing
+#aggregation
+#composition
+#nested classes
+#static methods
+#class methods
+#magic nethods
+#property decorator
+
+
